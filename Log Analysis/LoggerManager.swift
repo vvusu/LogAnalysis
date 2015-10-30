4bc6393ad28bc81b3df37b65478ffd0a467ede95
wedxz <wedxzl@gmail.com>
2015-10-30 13:57:28 +0800

    updtate log Calculating time

--- Log Analysis/LoggerManager.swift
+++ Log Analysis/LoggerManager.swift
@@ -11,6 +11,12 @@ import Cocoa
 //Instance
 class LoggerManager: NSObject {
     
+    var dataArr = [[Log]]()
+    //遍历转换model
+    var commonArr = [Log]()
+    var bookArr = [Log]()
+    var crashArr = [Log]()
+    
     class var sharedInstance : LoggerManager {
         struct Static {
             static var onceToken : dispatch_once_t = 0
@@ -23,16 +29,64 @@ class LoggerManager: NSObject {
     }
     
     func readFile(pathURL: NSURL) -> Array<[Log]> {
-        var dataArr = [[Log]]()
         do {
-            var mytext = try String(contentsOfURL: pathURL, encoding: NSUTF8StringEncoding)
-            //去除 \r\n \n  要不正则不能匹配值
-            mytext = mytext.stringByReplacingOccurrencesOfString("\r\n", withString:"")
-            mytext = mytext.stringByReplacingOccurrencesOfString("\n", withString:"")
+            let mytext = try String(contentsOfURL: pathURL, encoding: NSUTF8StringEncoding)
+            //截取按单个字符串
+//            logStrings = mytext.componentsSeparatedByString("{}")
+            //截取按多个字符串
+            let logStrings = mytext.componentsSeparatedByCharactersInSet(NSCharacterSet (charactersInString: "{}"))
+            var logJsons = [String]()
+            for num in  0..<logStrings.count {
+                if num % 2 == 1 {
+                        let logStr = logStrings[num]
+                        let logJson = "{" + logStr + "}"
+                        logJsons.append(logJson)
+                        // 日志model 处理
+                        if let tempLog: Log = jsonToModel(logJson) as? Log{
+                            if tempLog.partname == "public" {
+                                if tempLog.level == "2" {
+                                    if crashArr.count == 0 {
+                                        crashArr.append(tempLog)
+                                    } else {
+                                        if !judgingTheSameLog(tempLog, logArr: crashArr) {
+                                            crashArr.append(tempLog)
+                                        }
+                                    }
+                                }
+                                else {
+                                    if commonArr.count == 0 {
+                                        commonArr.append(tempLog)
+                                    } else {
+                                        if !judgingTheSameLog(tempLog, logArr: commonArr) {
+                                            commonArr.append(tempLog)
+                                        }
+                                    }
+                                }
+                            }
+                            else {
+                                if bookArr.count == 0 {
+                                    bookArr.append(tempLog)
+                                } else {
+                                    if !judgingTheSameLog(tempLog, logArr: bookArr) {
+                                        bookArr.append(tempLog)
+                                    }
+                                }
+                            }
+
+                        }
+                }
+            }
             
-             let logStrArr = self.regular(mytext)
-             //日志解析
-             dataArr = self.parsingLog(logStrArr)
+            if commonArr.count > 0 {
+                dataArr.append(commonArr)
+            }
+            if bookArr.count > 0 {
+                dataArr.append(bookArr)
+            }
+            if crashArr.count > 0 {
+                dataArr.append(crashArr)
+            }
+            return dataArr
 
         } catch let error as NSError {
             print("error loading from url \(pathURL)")
@@ -41,21 +95,22 @@ class LoggerManager: NSObject {
         return dataArr
     }
     
-    //正则表达式
-     func regular(str: String) -> Array<String> {
+    //正则表达式  （比较耗时间）1、创建规则 (获取{}里面的内容)  let pattern = "(\\{.+?\\})"
+    func regular(var text: String, pattern: String) -> Array<String> {
+        //去除 \r\n \n  要不正则不能匹配值
+        text = text.stringByReplacingOccurrencesOfString("\r\n", withString:"")
+        text = text.stringByReplacingOccurrencesOfString("\n", withString:"")
         // 使用正则表达式一定要加try语句
         var contenArr = [String]()
         do {
-            // - 1、创建规则 (获取{}里面的内容)
-            let pattern = "(\\{.+?\\})"
             // - 2、创建正则表达式对象
             let regex = try NSRegularExpression(pattern: pattern, options: NSRegularExpressionOptions.CaseInsensitive)
             // - 3、开始匹配
-            let res = regex.matchesInString(str, options: NSMatchingOptions(rawValue: 0), range: NSMakeRange(0, str.characters.count))
+            let res = regex.matchesInString(text, options: NSMatchingOptions(rawValue: 0), range: NSMakeRange(0, text.characters.count))
             // 输出结果
             var content: String
             for subRes in res {
-                content = str[subRes.range.location..<subRes.range.location+subRes.range.length]
+                content = text[subRes.range.location..<subRes.range.location+subRes.range.length]
                 contenArr.append(content)
             }
         }
@@ -76,59 +131,6 @@ class LoggerManager: NSObject {
         else {
             return nil
         }
-    }
-    
-    //循环遍历log 取出值
-    func parsingLog(logJsonArr: Array<String>) -> Array<[Log]> {
-        var dataArr = [[Log]]()
-        //遍历转换model
-        var commonArr = [Log]()
-        var bookArr = [Log]()
-        var crashArr = [Log]()
-
-        for json in logJsonArr {
-            if let tempLog: Log = jsonToModel(json) as? Log{
-                if tempLog.partname == "public" {
-                    if tempLog.level == "2" {
-                        if crashArr.count == 0 {
-                            crashArr.append(tempLog)
-                        } else {
-                            if !judgingTheSameLog(tempLog, logArr: crashArr) {
-                                crashArr.append(tempLog)
-                            }
-                        }
-                    }
-                    else {
-                        if commonArr.count == 0 {
-                            commonArr.append(tempLog)
-                        } else {
-                            if !judgingTheSameLog(tempLog, logArr: commonArr) {
-                                commonArr.append(tempLog)
-                            }
-                        }
-                    }
-                }
-                else {
-                    if bookArr.count == 0 {
-                        bookArr.append(tempLog)
-                    } else {
-                        if !judgingTheSameLog(tempLog, logArr: bookArr) {
-                           bookArr.append(tempLog)
-                        }
-                    }
-                }
-            }
-        }
-        if commonArr.count > 0 {
-            dataArr.append(commonArr)
-        }
-        if bookArr.count > 0 {
-            dataArr.append(bookArr)
-        }
-        if crashArr.count > 0 {
-            dataArr.append(crashArr)
-        }
-        return dataArr
     }
     
     func judgingTheSameLog(tempLog: Log, logArr: Array<Log>) -> Bool {
